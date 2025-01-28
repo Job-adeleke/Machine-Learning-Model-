f95f30365955bdb114f5ddfbede43c8dd3e19982
Job Adeleke <adelekejob3@gmail.com>
2025-01-29 01:12:56 +0300

    Add files via upload

---
+++ Building Linear Regression Model/Linear_Regression_Model.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+class Linear_Regression: 
+    # initiating the parameters (learning rate & no_of_iterations)
+    def __init__(self, learning_rate, no_of_iterations):
+        self.learning_rate = learning_rate
+        self.no_of_iterations = no_of_iterations
+
+    def fit(self, X, Y) :
+
+        # number of training examples (m) # number of features (n)
+
+        self.m, self.n = X.shape # number of rows & columns 
+
+        # initiating the weight and bias
+
+        self.w = np.zeros(self.n)
+        self.b = 0
+        self.X = X
+        self.Y = Y
+
+        # implementing gradient descent 
+        for i in range (self.no_of_iterations):
+            self.update_weights ()
+
+    def update_weights(self):
+        Y_prediction = self.predict(self.X)
+
+        # calculate the gradient 
+
+        dw = - (2 * (self.X.T).dot(self.Y - Y_prediction)) / self.m
+
+        db = - 2 * np.sum(self.Y - Y_prediction) / self.m
+
+        # updating the weights
+
+        self.w = self.w - self.learning_rate*dw
+        self.b = self.b - self.learning_rate*db
+        
+
+    def predict(self, X):
+
+        return X.dot(self.w) + self.b
